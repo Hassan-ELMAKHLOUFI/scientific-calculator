dfcf87ca6e3c54116b8efbc20a1d4a8348dcc2f6
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-03-09 17:14:35 +0100

    land

--- Calculatrice/app/src/main/java/com/example/calculatrice/MainActivity.java
+++ Calculatrice/app/src/main/java/com/example/calculatrice/MainActivity.java
@@ -1,81 +1,118 @@
 package com.example.calculatrice;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import org.mariuszgromada.*;
+import org.mariuszgromada.math.mxparser.Expression;
 
 public class MainActivity extends AppCompatActivity {
-    int valeur1 =0 ;
-    int valeur2 =0  ;
-    String operation  ;
-    boolean isOp1=true ;
-    TextView opt ;
+
+    TextView operation ;
+    EditText resultat ;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        opt = this.findViewById(R.id.operation);
-        opt.setText("0");
-
-
+        operation = this.findViewById(R.id.operation);
+        resultat = this.findViewById(R.id.resultat);
+        resultat.setShowSoftInputOnFocus(false);
 
 
     }
 
-    public void setOperator(View view){
-        isOp1=false ;
-        String val = opt.getText().toString();
-        Button btn = findViewById(view.getId());
-        operation = btn.getText().toString();
-        System.out.println(btn.getText().toString());
-        opt.setText(val+btn.getText().toString());
+    public void changer(String str){
+        String leftPos,rightPos;
+        String value=resultat.getText().toString();
+        int cursorPosition=resultat.getSelectionStart();
+        leftPos=value.substring(0,cursorPosition);
+        rightPos=value.substring(cursorPosition);
+        resultat.setText(leftPos+str+rightPos);
+        resultat.setSelection(cursorPosition+str.length());
 
 
+    }
+
+
+    public void zero(View view){
+        changer("0");
+    }
+    public void one(View view){
+        changer("1");
+    }
+    public void two(View view){
+        changer("2");
+    }
+    public void three(View view){
+        changer("3");
+    }
+    public void four(View view){
+        changer("4");
+    }
+    public void five(View view){
+        changer("5");
+    }
+    public void six(View view){
+        changer("6");
+    }
+    public void seven(View view){
+        changer("7");
+    }
+    public void eight(View view){
+        changer("8");
+    }
+    public void nine(View view){
+        changer("9");
+    }
+    public void mul(View view){
+        changer("*");
+    }
+    public void div(View view){
+        changer("/");
+    }
+    public void sous(View view){
+        changer("-");
+    }
+    public void add(View view){
+        changer("+");
+    }
+    public void delete(View view){
+       int curPos = resultat.getSelectionStart();
+       int stringSize = resultat.getText().length();
+
+       if(curPos != 0){
+           SpannableStringBuilder selection= (SpannableStringBuilder) resultat.getText();
+            selection.replace(curPos-1,curPos,"");
+       }
+    }
+    public void clear(View view){
+        resultat.setText("");
+    }
+    public void parClose(View view){
+        changer(")");
+    }
+    public void parOpen(View view){
+        changer("(");
+    }
+    public void Decimal(View view){
+        changer(".");
     }
     public void equal(View view){
-        int  result=10;
-
-        if(operation.equals("+")){
-            result=valeur1+valeur2;
-            opt.setText("");
-            opt.setText(""+result);
-        }
-        if(operation.equals("-")){
-            result=valeur1-valeur2;
-            opt.setText("");
-            opt.setText(""+result);
-            System.out.println("rs="+result);
-        }
-        if(operation.equals("X")){
-            result=valeur1*valeur2;
-            opt.setText("");
-            opt.setText(""+result);
-        }
-        if(operation.equals("/")){
-            result=valeur1/valeur2;
-            opt.setText("");
-            opt.setText(""+result);
-        }
-
+        String opt = resultat.getText().toString();
+        Expression exp= new Expression(opt);
+        String res=String.valueOf(exp.calculate());
+        resultat.setText(res);
+        resultat.setSelection(resultat.length());
     }
-    public void ajouterNbr(View view){
-        Button btn = findViewById(view.getId());
-        String val = opt.getText().toString();
 
 
-        if(isOp1){
-            valeur1= valeur1*10+Integer.parseInt(btn.getText().toString()) ;
-        }
-        if(!isOp1){
-            valeur2= valeur2*10+Integer.parseInt(btn.getText().toString()) ;
-        }
-
-        if(val.equals("0")){
-            opt.setText(""+btn.getText().toString());
-        }else{
-        opt.setText(val+btn.getText().toString());}
-    }
 }
